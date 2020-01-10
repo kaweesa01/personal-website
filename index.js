749fc9c6a331fa0f77615d6b95756aeccc5bda18
kaweesa01 <42151017+kaweesa01@users.noreply.github.com>
2020-01-10 07:38:40 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const img = document.querySelector('.image_design');
-function enlarge(){
-    img.classList.toggle('enlarge')
+const display = document.querySelector('.container_nav');
+const mobileNav = document.querySelector('.mobile_nav');
+const mobile_list = document.querySelector('.mobile_list_container');
+const webName = document.querySelector('.move_nav');
+const alignwebname = document.querySelector('.web_name');
+const display_exite = document.querySelectorAll('.mobile_nav_links');
+const playing_list = document.querySelectorAll('.list_nav');
+
+for (let list = 0; list < playing_list.length; list++) {
+     
+function add_player(){
+     playing_list[list].classList.toggle('player');
+ }
+    playing_list[list].addEventListener('click',add_player)
 }
-img.addEventListener('click',enlarge);
+
+    
+
+
+
+function remove_class(){
+     mobileNav.classList.remove('bigger_mobile_nav');
+     alignwebname.classList.remove('align_web_name');
+     webName.classList.remove('margin_web_name');
+     mobileNav.classList.remove('bigger_mobile_nav');
+     display.classList.remove('top_container_nav');
+     mobile_list.classList.remove('bottom_mobile_list_container');
+   
+     
+}
+
+for (let index = 0; index < display_exite.length; index++) {
+    display_exite[index].addEventListener('click', remove_class)
+     
+}
+function addHeight(){
+     alignwebname.classList.toggle('align_web_name');
+     webName.classList.toggle('margin_web_name');
+     mobileNav.classList.toggle('bigger_mobile_nav');
+     display.classList.toggle('top_container_nav');
+     mobile_list.classList.toggle('bottom_mobile_list_container');
+
+    
+   
+}
+display.addEventListener('click', addHeight);
